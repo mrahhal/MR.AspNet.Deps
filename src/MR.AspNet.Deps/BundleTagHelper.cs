d57af7db3c829f63251dc3771b46778d78fb61dc
Mohammad Rahhal <m.r992@hotmail.com>
2015-12-01 15:34:38 +0200

    + BundleTagHelper

---
+++ src/MR.AspNet.Deps/BundleTagHelper.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace MR.AspNet.Deps
+{
+	public class BundleTagHelper : TagHelper
+	{
+		public BundleTagHelper(DepsManager depsManager)
+		{
+			DepsManager = depsManager;
+		}
+
+		public string Name { get; set; }
+
+		public BundleKind Kind { get; set; }
+
+		private DepsManager DepsManager { get; set; }
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = null;
+			switch (Kind)
+			{
+				case BundleKind.Script:
+					output.PreContent.SetContent(DepsManager.RenderJs(Name));
+					break;
+
+				case BundleKind.Style:
+					output.PreContent.SetContent(DepsManager.RenderCss(Name));
+					break;
+			}
+		}
+	}
+}
