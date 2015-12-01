b36a1f5273a93d98a1ae7fbba9bd88d0c6eb47fa
Mohammad Rahhal <m.r992@hotmail.com>
2015-12-01 15:33:44 +0200

    Initial commit

---
+++ src/MR.AspNet.Deps/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyProduct("MR.AspNet.Deps")]
+[assembly: AssemblyTitle("MR.AspNet.Deps")]
+[assembly: AssemblyDescription("An opinionated library that manages client side resources in AspNet5 (js, css, scss, ...)")]
+[assembly: AssemblyCompany("MRH")]
+[assembly: AssemblyCopyright("Copyright (c) 2015")]
