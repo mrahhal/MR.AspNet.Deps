8ff55a21094bdacbde474c489e0dac225e67ed30
Mohammad Rahhal <m.r992@hotmail.com>
2015-12-02 23:51:36 +0200

    Version -> beta2

--- src/MR.AspNet.Deps/Properties/AssemblyInfo.cs
+++ src/MR.AspNet.Deps/Properties/AssemblyInfo.cs
@@ -2,6 +2,3 @@
 
 [assembly: AssemblyProduct("MR.AspNet.Deps")]
 [assembly: AssemblyTitle("MR.AspNet.Deps")]
-[assembly: AssemblyDescription("An opinionated library that manages client side resources in AspNet5 (js, css, scss, ...)")]
-[assembly: AssemblyCompany("MRH")]
-[assembly: AssemblyCopyright("Copyright (c) 2015")]
