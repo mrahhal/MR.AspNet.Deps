3bde0d5948084d3f0f52919a48c07a2eb4a9747d
Mohammad Rahhal <m.r992@hotmail.com>
2015-12-15 18:04:47 +0200

    Only expose needed sections in Deps

--- src/MR.AspNet.Deps/Deps.cs
+++ src/MR.AspNet.Deps/Deps.cs
@@ -4,9 +4,7 @@ namespace MR.AspNet.Deps
 {
 	public class Deps
 	{
-		public IList<string> Fonts { get; set; }
 		public IList<Bundle> Css { get; set; }
 		public IList<Bundle> Js { get; set; }
-		public IList<Bundle> Sass { get; set; }
 	}
 }
