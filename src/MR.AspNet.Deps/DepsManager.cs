a8a273a32e0095c6a6a5c83d74ebaca593d192aa
Mohammad Rahhal <m.r992@hotmail.com>
2015-12-15 17:24:10 +0200

    Use a local FileVersionProvider to produce version hashes

--- src/MR.AspNet.Deps/DepsManager.cs
+++ src/MR.AspNet.Deps/DepsManager.cs
@@ -5,9 +5,14 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
 using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+using Microsoft.AspNet.Razor.TagHelpers;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.OptionsModel;
 using Microsoft.Extensions.PlatformAbstractions;
+using MR.AspNet.Deps.Microsoft.AspNet.Mvc.TagHelpers.Internal;
 using Newtonsoft.Json;
 
 namespace MR.AspNet.Deps
@@ -17,23 +22,42 @@ namespace MR.AspNet.Deps
 		private ConcurrentDictionary<string, string[]> _cache = new ConcurrentDictionary<string, string[]>();
 		private IApplicationEnvironment _appEnv;
 		private IHostingEnvironment _env;
+		private IMemoryCache _memoryCache;
 		private DepsOptions _options;
 		private Deps deps;
 		private Uri _relative;
+		private FileVersionProvider _fileVersionProvider;
 
 		public DepsManager(
 			IApplicationEnvironment appEnv,
 			IHostingEnvironment env,
+			IMemoryCache memoryCache,
 			IOptions<DepsOptions> options)
 		{
 			_appEnv = appEnv;
 			_env = env;
+			_memoryCache = memoryCache;
 			_options = options.Value;
 			_relative = new Uri(Path.Combine(_appEnv.ApplicationBasePath, "wwwroot/"));
 
 			if (_options.Cache || !_env.IsDevelopment())
 			{
 				LoadDeps();
+			}
+		}
+
+		[HtmlAttributeNotBound]
+		[ViewContext]
+		public ViewContext ViewContext { get; set; }
+
+		private void EnsureFileVersionProvider()
+		{
+			if (_fileVersionProvider == null)
+			{
+				_fileVersionProvider = new FileVersionProvider(
+					_env.WebRootFileProvider,
+					_memoryCache,
+					ViewContext?.HttpContext.Request.PathBase ?? new PathString());
 			}
 		}
 
@@ -136,14 +160,20 @@ namespace MR.AspNet.Deps
 
 		private string CreateScriptTag(string src, bool appendVersion)
 		{
-			var appendText = GetAppendText(appendVersion);
-			return $"<script src=\"{src}\" {appendText}></script>";
+			src = appendVersion ? AppendVersion(src) : src;
+			return $"<script src=\"{src}\"></script>";
 		}
 
 		private string CreateLinkTag(string href, bool appendVersion)
 		{
-			var appendText = GetAppendText(appendVersion);
-			return $"<link rel=\"stylesheet\" href=\"{href}\" {appendText} />";
+			href = appendVersion ? AppendVersion(href) : href;
+			return $"<link rel=\"stylesheet\" href=\"{href}\" />";
+		}
+
+		private string AppendVersion(string path)
+		{
+			EnsureFileVersionProvider();
+			return _fileVersionProvider.AddFileVersionToPath(path);
 		}
 
 		private string GetKeyForBundle(string bundle, BundleKind kind)
@@ -152,16 +182,13 @@ namespace MR.AspNet.Deps
 			{
 				case BundleKind.Script:
 					return "sc_" + bundle;
+
 				case BundleKind.Style:
 					return "st_" + bundle;
+
 				default:
 					return null;
 			}
-		}
-
-		private string GetAppendText(bool appendVersion)
-		{
-			return appendVersion ? "asp-append-version=\"true\"" : string.Empty;
 		}
 	}
 }
---
+++ src/MR.AspNet.Deps/FileVersionProvider.cs
@@ -0,0 +1,119 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Security.Cryptography;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.WebUtilities;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace MR.AspNet.Deps.Microsoft.AspNet.Mvc.TagHelpers.Internal
+{
+	/// <summary>
+	/// Provides version hash for a specified file.
+	/// </summary>
+	public class FileVersionProvider
+	{
+		private const string VersionKey = "v";
+		private readonly IFileProvider _fileProvider;
+		private readonly IMemoryCache _cache;
+		private readonly PathString _requestPathBase;
+
+		/// <summary>
+		/// Creates a new instance of <see cref="FileVersionProvider"/>.
+		/// </summary>
+		/// <param name="fileProvider">The file provider to get and watch files.</param>
+		/// <param name="applicationName">Name of the application.</param>
+		/// <param name="cache"><see cref="IMemoryCache"/> where versioned urls of files are cached.</param>
+		public FileVersionProvider(
+			IFileProvider fileProvider,
+			IMemoryCache cache,
+			PathString requestPathBase)
+		{
+			if (fileProvider == null)
+			{
+				throw new ArgumentNullException(nameof(fileProvider));
+			}
+
+			if (cache == null)
+			{
+				throw new ArgumentNullException(nameof(cache));
+			}
+
+			_fileProvider = fileProvider;
+			_cache = cache;
+			_requestPathBase = requestPathBase;
+		}
+
+		/// <summary>
+		/// Adds version query parameter to the specified file path.
+		/// </summary>
+		/// <param name="path">The path of the file to which version should be added.</param>
+		/// <returns>Path containing the version query string.</returns>
+		/// <remarks>
+		/// The version query string is appended as with the key "v".
+		/// </remarks>
+		public string AddFileVersionToPath(string path)
+		{
+			if (path == null)
+			{
+				throw new ArgumentNullException(nameof(path));
+			}
+
+			var resolvedPath = path;
+
+			var queryStringOrFragmentStartIndex = path.IndexOfAny(new char[] { '?', '#' });
+			if (queryStringOrFragmentStartIndex != -1)
+			{
+				resolvedPath = path.Substring(0, queryStringOrFragmentStartIndex);
+			}
+
+			Uri uri;
+			if (Uri.TryCreate(resolvedPath, UriKind.Absolute, out uri) && !uri.IsFile)
+			{
+				// Don't append version if the path is absolute.
+				return path;
+			}
+
+			var fileInfo = _fileProvider.GetFileInfo(resolvedPath);
+			if (!fileInfo.Exists)
+			{
+				if (_requestPathBase.HasValue &&
+					resolvedPath.StartsWith(_requestPathBase.Value, StringComparison.OrdinalIgnoreCase))
+				{
+					resolvedPath = resolvedPath.Substring(_requestPathBase.Value.Length);
+					fileInfo = _fileProvider.GetFileInfo(resolvedPath);
+				}
+
+				if (!fileInfo.Exists)
+				{
+					// if the file is not in the current server.
+					return path;
+				}
+			}
+
+			string value;
+			if (!_cache.TryGetValue(path, out value))
+			{
+				value = QueryHelpers.AddQueryString(path, VersionKey, GetHashForFile(fileInfo));
+				var cacheEntryOptions = new MemoryCacheEntryOptions().AddExpirationToken(_fileProvider.Watch(resolvedPath));
+				_cache.Set(path, value, cacheEntryOptions);
+			}
+
+			return value;
+		}
+
+		private string GetHashForFile(IFileInfo fileInfo)
+		{
+			using (var sha256 = SHA256.Create())
+			{
+				using (var readStream = fileInfo.CreateReadStream())
+				{
+					var hash = sha256.ComputeHash(readStream);
+					return WebEncoders.Base64UrlEncode(hash);
+				}
+			}
+		}
+	}
+}
