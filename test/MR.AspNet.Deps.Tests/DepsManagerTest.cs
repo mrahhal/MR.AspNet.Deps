3bc4fe910b4af54a729959cfae4de4cdbd04be44
Mohammad Rahhal <m.r992@hotmail.com>
2015-12-15 19:56:46 +0200

    Setup the test proj

---
+++ test/MR.AspNet.Deps.Tests/DepsManagerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Moq;
+using Xunit;
+
+namespace MR.AspNet.Deps.Tests
+{
+	public class DepsManagerTest
+	{
+		[Fact]
+		public void CanCreate()
+		{
+			var services = CreateDefaultServiceCollection();
+			var appEnv = new Mock<IApplicationEnvironment>();
+			appEnv.Setup(m => m.ApplicationBasePath).Returns("C:\\project");
+			services.AddInstance(appEnv.Object);
+			services.AddInstance(new Mock<IHostingEnvironment>().Object);
+			services.AddInstance(new Mock<IAppRootFileProviderAccessor>().Object);
+			var provider = services.BuildServiceProvider();
+
+			var manager = provider.GetService<DepsManager>();
+
+			Assert.NotNull(manager);
+		}
+
+		private ServiceCollection CreateDefaultServiceCollection(Action<DepsManager> configureOptions = null)
+		{
+			var services = new ServiceCollection();
+			services.AddOptions();
+			services.AddInstance(new Mock<IMemoryCache>().Object);
+			services.Configure(configureOptions ?? ((_) => { }));
+			services.AddSingleton<DepsManager>();
+			return services;
+		}
+	}
+}
