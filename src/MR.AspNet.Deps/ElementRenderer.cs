02fdbbafd38c46fe15df36f30dbbfda8164144ff
Mohammad Rahhal <m.r992@hotmail.com>
2015-12-19 17:48:23 +0200

    + ElementRenderer to render resulted elements

---
+++ src/MR.AspNet.Deps/ElementRenderer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNet.Mvc.Rendering;
+
+namespace MR.AspNet.Deps
+{
+	public class ElementRenderer
+	{
+		public HtmlString Render(IList<Element> elements)
+		{
+			if (elements == null)
+			{
+				throw new ArgumentNullException(nameof(elements));
+			}
+
+			var sb = new StringBuilder();
+			foreach (var element in elements)
+			{
+				sb.Append(RenderCore(element));
+			}
+
+			return new HtmlString(sb.ToString());
+		}
+
+		public HtmlString Render(Element element)
+		{
+			if (element == null)
+			{
+				throw new ArgumentNullException(nameof(element));
+			}
+
+			return new HtmlString(RenderCore(element));
+		}
+
+		private string RenderCore(Element element)
+		{
+			var sb = new StringBuilder();
+
+			sb.Append($"<{element.TagName}");
+
+			if (element.Attributes.Any())
+			{
+				sb.Append(" ");
+			}
+
+			for (int i = 0; i < element.Attributes.Count; i++)
+			{
+				var attribute = element.Attributes[i];
+				sb.Append($"{attribute.Name}=\"{attribute.Value}\"");
+				if (i != element.Attributes.Count - 1)
+				{
+					sb.Append(" ");
+				}
+			}
+
+			if (!string.IsNullOrWhiteSpace(element.Content) && element.ClosingTagKind != ClosingTagKind.Normal)
+			{
+				throw new InvalidOperationException(
+					"Elements with a non-normal closing tag kind cannot have content.");
+			}
+
+			switch (element.ClosingTagKind)
+			{
+				case ClosingTagKind.None:
+					sb.Append(" >");
+					break;
+				case ClosingTagKind.SelfClosing:
+					sb.Append(" />");
+					break;
+				case ClosingTagKind.Normal:
+					sb.Append(">");
+					if (!string.IsNullOrWhiteSpace(element.Content))
+					{
+						sb.Append(element.Content);
+					}
+					sb.Append($"</{element.TagName}>");
+					break;
+			}
+
+			return sb.ToString();
+		}
+	}
+}
---
+++ test/MR.AspNet.Deps.Tests/ElementRendererTest.cs
@@ -0,0 +1,65 @@
+﻿using Xunit;
+
+namespace MR.AspNet.Deps.Tests
+{
+	public class ElementRendererTest
+	{
+		[Fact]
+		public void Render_KindNone()
+		{
+			var renderer = new ElementRenderer();
+			var e = new Element("tag", ClosingTagKind.None);
+
+			var result = renderer.Render(e).ToString();
+
+			Assert.Equal("<tag >", result);
+		}
+
+		[Fact]
+		public void Render_KindSelfClosing()
+		{
+			var renderer = new ElementRenderer();
+			var e = new Element("tag", ClosingTagKind.SelfClosing);
+
+			var result = renderer.Render(e).ToString();
+
+			Assert.Equal("<tag />", result);
+		}
+
+		[Fact]
+		public void Render_KindNormal()
+		{
+			var renderer = new ElementRenderer();
+			var e = new Element("tag", ClosingTagKind.Normal);
+
+			var result = renderer.Render(e).ToString();
+
+			Assert.Equal("<tag></tag>", result);
+		}
+
+		[Fact]
+		public void Render_WithAttribute()
+		{
+			var renderer = new ElementRenderer();
+			var e = new Element("tag", ClosingTagKind.Normal);
+			e.Attributes.Add(new ElementAttribute("foo", "bar"));
+
+			var result = renderer.Render(e).ToString();
+
+			Assert.Equal("<tag foo=\"bar\"></tag>", result);
+		}
+
+		[Fact]
+		public void Render_WithAttributes()
+		{
+			var renderer = new ElementRenderer();
+			var e = new Element("tag", ClosingTagKind.Normal);
+			e.Attributes.Add(new ElementAttribute("foo", "bar"));
+			e.Attributes.Add(new ElementAttribute("baz", "some"));
+
+			var result = renderer.Render(e).ToString();
+
+			Assert.Equal("<tag foo=\"bar\" baz=\"some\"></tag>", result);
+		}
+	}
+}
